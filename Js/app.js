46876a5750a4164824a3c35bb287e13d9171a2bb
jobm <jobmwaniki18@gmail.com>
2015-10-26 12:23:39 +0300

    added file write script

--- Js/app.js
+++ Js/app.js
@@ -1,3 +1,14 @@
-$(function(){
-  alert("Hello there");
+$(function () {
+  function writeToFile(emial){
+    count = 0;
+    var fso = new ActiveXObject("Scripting.FileSystemObject");
+    var fh = fso.OpenTextFile("/emails.txt", 8, false, 0);
+    fh.WriteLine('Email Address '+' : '+email);
+    fh.Close();
+  }
+$('#subscribe').click(function(e){
+  e.preventDefault();
+  var email = $('#email-area').val();
+  writeToFile(email);
+});
 });
