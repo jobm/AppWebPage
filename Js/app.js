a3f4577b6846d16d0bdb35c33dd043633850aa3e
jobm <jobmwaniki18@gmail.com>
2015-10-26 13:03:02 +0300

    added another image

--- Js/app.js
+++ Js/app.js
@@ -2,13 +2,13 @@ $(function () {
   function writeToFile(emial){
     count = 0;
     var fso = new ActiveXObject("Scripting.FileSystemObject");
-    var fh = fso.OpenTextFile("/emails.txt", 8, false, 0);
+    var fh = fso.OpenTextFile("emails.txt", 8, false, 0);
     fh.WriteLine('Email Address '+' : '+email);
     fh.Close();
   }
 $('#subscribe').click(function(e){
-  e.preventDefault();
+  // e.preventDefault();
   var email = $('#email-area').val();
-  writeToFile(email);
+  writeToFile('email');
 });
 });
